7a1b0cc0812fe1d8cfa3dc8d9582c14fd34e2d99
Jude666 <zh415944@dal.ca>
2019-02-06 13:57:01 -0800

    task1

--- task1.py
+++ task1.py
@@ -1,13 +1,38 @@
 #This is task1
 
 f = open('Street_Centrelines.csv', 'r')
-
+f = readline()
 '''Tuple for names'''
 def tuple_name():
-    for line in file:
+    for line in f:
         line = line.split(',')
         name = (line[2], line[4], line[6], line[7])
         print(name)
+
+'''Histogram for maintenance'''
+def histogram():
+    d = dict()
+    for line in f:
+        line = line.split(',')
+        c = (line[12])
+        if c not in d:
+            d[c] = 1
+        else:
+            d[c] += 1
+    print(d)
+
+'''List of unique owners for the streets'''
+def owner_list():
+    ownerList = []
+    for line in f:
+        line = line.split(',')
+        i = line[11]
+        if i not in ownerList:
+            ownerList.append(i)
+    print(ownerList)
+
+'''Different types of street classes and list'''
+
 
 
 
